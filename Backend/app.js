8598440fe7a0dd1b90a41da4f7560ed5fcdc248e
Riza Sakhi <rizasakhi3@gmail.com>
2024-06-11 10:10:30 +0700

    backend things

--- Backend/app.js
+++ Backend/app.js
@@ -1,26 +1,45 @@
 const express = require("express");
 const app = express();
-const bodyParser = (require('body-parser'))
+const bodyParser = (require('body-parser'));
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number
+})
+
+const Product = mongoose.model('Product', productSchema);
 
 require('dotenv/config');
 
 const api = process.env.API_URL
 
 app.get(`${api}/products`, (req, res) =>{
-    const products = {
+    const product = {
         id : 1,
         name: riza,
         image : 'url',
     }
-    res.send(products);
+    res.send(product);
 })
 
 app.post(`${api}/products`, (req, res) =>{
     const newProduct = req.body;
     console.log(newProduct);
     res.send(products);
+})
+
+mongoose.connect(process.env.CONNECTION_STRING)
+.then(()=>{
+    console.log('Database connection is ready..')
+})
+.catch((err)=>{
+    console.log(err);
 })
 
 app.listen(3000, ()=>{
