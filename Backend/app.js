e1ba7dc928b37361caacc9982d56ac4320fe35f6
Riza Sakhi <rizasakhi3@gmail.com>
2024-06-19 12:47:26 +0700

    mengubah file-file backend

--- Backend/app.js
+++ Backend/app.js
@@ -4,51 +4,25 @@ const bodyParser = (require('body-parser'));
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
+require('dotenv/config');
+
+
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true
-    }
-})
-
-const Product = require('./models/product')
-
-require('dotenv/config');
+//router
+const productsRouter = require('./routers/products');
+const categoriesRouter = require('./routers/categories');
+const usersRouter = require('./routers/users');
+const ordersRouter = require('./routers/orders');
 
 const api = process.env.API_URL
 
-app.get(`${api}/products`, async (req, res) =>{
-    const productList = await Product.find();
-
-    if(!productList){
-        res.status(500).json({success: false})
-    }
-    
-    res.send(productList);
-})
-
-app.post(`${api}/products`, (req, res) =>{
-    const product =  new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save().then((createdProduct=>{
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-})
+app.use(`${api}/products`, productsRouter);
+app.use(`${api}/categories`, categoriesRouter);
+app.use(`${api}/users`, usersRouter);
+app.use(`${api}/orders`, ordersRouter);
 
 mongoose.connect(process.env.CONNECTION_STRING)
 .then(()=>{
